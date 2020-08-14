957b7efe930c0f3915a9b90c60fde0f69ade5106
Patrick Dreier <patrick3dreier@googlemail.com>
2020-08-14 13:05:57 +0200

    added photo functionality

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -27,7 +27,7 @@ class CocktailsController < ApplicationController
   end
 
   def strong_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 
 end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,6 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :delete_all
   has_many :reviews, dependent: :delete_all
   has_many :ingredients, through: :doses
+  has_one_attached :photo
   validates :name, presence: true, uniqueness: true
 end
